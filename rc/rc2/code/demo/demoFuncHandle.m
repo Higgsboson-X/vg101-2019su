ecd26ee524ed4cdc76986ae34c5beb71549ebf73
0x4368-0x7877 <30561722+Higgsboson-X@users.noreply.github.com>
2019-05-27 10:23:19 +0800

    Updated demo

--- rc/rc2/code/demo/demoFuncHandle.m
+++ rc/rc2/code/demo/demoFuncHandle.m
@@ -1,11 +1,12 @@
 clearvars;
-
+% pass a function as an argument;
 a = [2, 3, 1, 2, 4, 5, 8, 9, 2];
 lessFunc = @myLess;
 sorted = mySort(a, lessFunc);
 
 disp(sorted);
 
+% access a local function;
 hd = mainFuncTools;
 
 s = hd.mySum(1, 2);
